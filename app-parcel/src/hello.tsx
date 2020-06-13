ca24a373830064ae29ef5a76eaf3642b765ca2b5
David Reche <david.reche@intersystems.com>
2020-06-13 17:42:03 +0200

    Bundle parcel

--- app-parcel/src/hello.tsx
+++ app-parcel/src/hello.tsx
@@ -1,10 +1,7 @@
 import React from "react";
 
 export const HelloComponent = () => (
-  <div className="colorized">
-    <h1>Parcel Funciona !!</h1>
-    <div className="container">
-      <img src="./content/animated.gif" />
-    </div>
+  <div>
+    <h1>Hola desde React !!</h1>
   </div>
 );
